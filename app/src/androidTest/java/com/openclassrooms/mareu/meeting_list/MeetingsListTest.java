7005f45cd92f4eb6c42e8acfc0eb83c9309bc4fd
Ben_Linux <benoit.vincent2707@gmail.com>
2022-05-27 14:54:41 +0200

    Edit instrumented tests (add, update OK) ext : filters date & room

--- app/src/androidTest/java/com/openclassrooms/mareu/meeting_list/MeetingsListTest.java
+++ app/src/androidTest/java/com/openclassrooms/mareu/meeting_list/MeetingsListTest.java
@@ -2,11 +2,15 @@
 package com.openclassrooms.mareu.meeting_list;
 
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.openclassrooms.mareu.R;
 import com.openclassrooms.mareu.di.DI;
@@ -14,23 +18,30 @@ import com.openclassrooms.mareu.model.Meeting;
 import com.openclassrooms.mareu.ui.meeting_list.ListMeetingActivity;
 import com.openclassrooms.mareu.utils.DeleteViewAction;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.mareu.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
+import java.util.Calendar;
 
 
 /**
@@ -39,8 +50,8 @@ import static org.hamcrest.core.IsNull.notNullValue;
 @RunWith(AndroidJUnit4.class)
 public class MeetingsListTest {
 
-    // This is fixed
-    private static int ITEMS_COUNT = 7;
+
+    private int ITEMS_COUNT = 7;
 
     private ListMeetingActivity mActivity;
 
@@ -140,5 +151,97 @@ public class MeetingsListTest {
         // Check if meeting data (here subject) is displayed in the right field
         Meeting meetingPosition = DI.getMeetingApiService().getMeetings().get(4);
         onView(withId(R.id.add_subject_input)).check(matches(withText(meetingPosition.getSubject())));
+    }
+
+    /**
+     * When we add a meeting, the item is in the meetings list
+     * DONT' RUN this test alone (fails because includes item count with item deleted before)
+     */
+    @Test
+    public void myMeetingsList_addingNewMeetingAction_shouldAddItem() {
+        // Check meetings list count (6, because of deleting test before)
+        onView(withId(R.id.list_meetings)).check(withItemCount(ITEMS_COUNT-1));
+        // Click on the creation button to add a new meeting
+        onView(withId(R.id.add_meeting))
+                .perform(click());
+        // Click on the date field
+        onView(withId(R.id.add_date_input))
+                .perform(click());
+        // Set meeting date (day / mounth / year)
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2022,  5, 19));
+        // Click OK button
+        onView(withId(android.R.id.button1)).perform(click());
+        // Set meeting hour (hour / minute)
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(14,  0));
+        // Click OK button
+        onView(withId(android.R.id.button1)).perform(click());
+        // Click on the subject field
+        onView(withId(R.id.add_subject_input))
+                .perform(click());
+        // Enter meeting's subject
+        onView(withId(R.id.add_subject_input))
+                .perform(typeText("New Meeting"), closeSoftKeyboard());
+        // Click on 2nd meeting room radio button ("Mario")
+        onView(withId(R.id.radioButton_room2))
+                .perform(click());
+        // Click on the description field
+        onView(withId(R.id.add_description))
+                .perform(click());
+        // Enter meeting's description
+        onView(withId(R.id.add_description))
+                .perform(typeText("This is my meeting description"));
+        // Click on the participants field
+        onView(withId(R.id.add_participants_input))
+                .perform(click());
+        // Enter a participant email (checked with email regex)
+        onView(withId(R.id.add_participants_input))
+                .perform(typeText("participant@test.com"), pressImeActionButton());
+        // Click on the creation button to add this new meeting
+        onView(withId(R.id.create))
+                .perform(scrollTo(), click());
+        // Then : check if item was added to the list ()
+        onView(withId(R.id.list_meetings)).check(withItemCount(ITEMS_COUNT));
+    }
+
+    /**
+     * When we add a meeting, the item is in the meetings list
+     * DONT' RUN this test alone (fails because includes item count with item deleted before)
+     */
+    @Test
+    public void myMeetingsDetails_editingNewMeetingAction_shouldUpdateItem() {
+        // Click on the fifth item in the meetings list
+        onView(withId(R.id.list_meetings))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(4, click()));
+        // Check if meeting details page is displayed
+        onView(withId(R.id.meeting_page)).check(matches(isDisplayed()));
+        // When perform a click on the update button
+        onView(withId(R.id.meeting_detail_update_button)).perform(click());
+        // Check if alert dialog is displayed
+        onView(withText("Voulez vous vraiment modifier cette réunion ?")).check(matches(isDisplayed()));
+        // When we click "OK" confirmation button
+        onView(withId(android.R.id.button1)).perform(click());
+        // Check if update meeting form is displayed
+        onView(withId(R.id.meeting_fields_page)).check(matches(isDisplayed()));
+        // Check if meeting data (here subject) is displayed in the right field
+        Meeting meetingPosition = DI.getMeetingApiService().getMeetings().get(4);
+        onView(withId(R.id.add_subject_input)).check(matches(withText(meetingPosition.getSubject())));
+        // Click on the date field
+        onView(withId(R.id.add_date_input))
+                .perform(click());
+        // Set meeting date (day / mounth / year)
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2022,  5, 19));
+        // Click OK button
+        onView(withId(android.R.id.button1)).perform(click());
+        // Set meeting hour (hour / minute)
+        onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(14,  0));
+        // Click OK button
+        onView(withId(android.R.id.button1)).perform(click());
+        // Click on the creation button to add this new meeting
+        onView(withId(R.id.create))
+                .perform(scrollTo(), click());
+        // Check if meeting details page is displayed
+        onView(withId(R.id.meeting_page)).check(matches(isDisplayed()));
+        // Check if meeting data (here date) is displayed and formatted in the right field
+        onView(withId(R.id.meeting_detail_date)).check(matches(withText("Le 19/05/2022 à 14:00")));
     }
 }
